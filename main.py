c4b323654dee713e2b30c2df9a1eff5b00e8b3b5
Ryan Lin <rlin2@andrew.cmu.edu>
2024-02-05 21:38:26 -0500

    Second commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+	num = 4
+	if num == 0: return 'Even'
+	if num == 1: return 'Odd'
+	if num == 2: return 'Even'
+	if num == 3: return 'Odd'
+	if num == 4: return 'Even'
+	return 'Odd'
+        
+if __name__ == "__main__":
+    	main()
