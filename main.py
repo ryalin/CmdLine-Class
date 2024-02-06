191c316b30adb53a48045546a2f1c406bcb839e0
Ryan Lin <rlin2@andrew.cmu.edu>
2024-02-05 21:35:39 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+	num = 4
+	if num == 0: return 'Even'
+	if num == 1: return 'Odd'
+	if num == 2: return 'Even'
+	if num == 3: return 'Odd'
+	if num == 4: return 'Even'
+        
+if __name__ == "__main__":
+    	main()
